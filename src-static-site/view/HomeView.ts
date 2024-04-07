66dd6a23e3a12f63bc8d47c5173060eeead9a71d
yjgaia <hanul@hanul.me>
2024-04-07 13:06:54 +0900

    dev

--- src-static-site/view/HomeView.ts
+++ src-static-site/view/HomeView.ts
@@ -129,7 +129,7 @@ export default class HomeView extends StaticDomNode {
             "ul.titles",
             el(
               "li",
-              "Builder",
+              "Member",
               el("span.date", " (2024.01 ~ )"),
             ),
           ),
